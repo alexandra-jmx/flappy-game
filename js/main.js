2d57f87d4a959e1b8fceed9e328693080f804ff4
alexandra-jmx <alexandra.jmx@gmail.com>
2023-06-21 15:48:56 -0300

    background day + comentários

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+import PreGameScene from './scene_pre-game.js';
+import GameScene from './scene_game.js';
 
 //Configurações 
 const configurations = {
     type: Phaser.AUTO,
     width: 288,
     height: 512,
+    parent: 'flappy-game',
     physics: {
         default: 'arcade', 
         arcade: {
             gravity: {
-                y: 300
+                y: 300 // testar qual seria mais adequada para uma criança hehe
             },
             debug: true
         }
@@ -18,3 +21,9 @@ const configurations = {
         GameScene
     ]
 }
+
+const game = new Phaser.Game(configurations);
+//Variáveis scenario
+let backgroundDay;
+let backgroundNight;
+let ground;
--- js/scene_pre-game.js
+++ js/scene_pre-game.js
@@ -5,15 +5,54 @@ export default class LoadingScene extends Phaser.Scene {
             key: 'PreGameScene'
         })
     }
+ 
+    
 
     preload() {
 
+        const scenario = {
+            width: 144,
+            background: {
+                day: 'background-day'
+                //night: 'background-night'
+            }, 
+            ground: 'ground-sprite',
+            obstacle: {
+                pipe: {
+                    green: {
+                        top: 'pipe-green-top',
+                        bottom: 'pipe-green-bottom'
+                    }//, red: {top: 'pipe-red-top', bottom: 'pipe-red-bottom'}
+                }
+            }
+        }
+        
+        
+        // Load the scenario
+        // - Day
+        //this.load.image('background-day', './assets/background-day.png');
+        this.load.image(scenario.background.day, './assets/background-day.png');
+        
+        // - Night
+        //this.load.image(scenario.background.night, 'assets/background-night.png');
 
+        // - Ground (Usado Sprite para o chão em função da animação)
+        /*this.load.spritesheet(scenario.ground, '.assets/ground-sprite.png' 
+            { frameWidth: 336,
+            frameHeight: 112 }
+         Também não dá certo senhor jesus cristo   )*/
     }
     
         create() {
-           
-    
+        backgroundDay = this.add.image(144, 256, 'background-day');
+        //backgroundDay = this.add.image(scenario.width, 256, 'background-day'); //Assim fica tudo preto
+
+        //backgroundNight
+        //ground = this.add.sprite(scenario.width, 458, 'ground-sprite');
+        /*ground.setCollideWorldBounds(true)
+        ground.setDepth(10)
+            */
+
     
         }
     
